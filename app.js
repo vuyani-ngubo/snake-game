eb8e77fdc020e59937a836e337ce22a73372af65
vuyani <ngubovuyani@gmail.com>
2017-05-25 01:34:52 +0200

    Implemented game-play interactivity, also defined refresh logic.

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const canvas = document.querySelector("#board");
 const context = canvas.getContext("2d");
 
-const snake = {
+document.addEventListener("keydown", ({ key }) => {
+	if (key === "ArrowUp") changeDirection("up");
+	else if (key === "ArrowDown") changeDirection("down");
+	else if (key === "ArrowLeft") changeDirection("left");
+	else if (key === "ArrowRight") changeDirection("right");
+	else if (key === " ") {
+		if (isPaused) play();
+		else pause();
+	}
+});
+
+var snake = {
 	body: [
 		{
 			x: 27,
@@ -25,7 +36,20 @@ const snake = {
 		},
 	],
 };
-const apple = randomPos();
+var apple = randomPos();
+var vector = {
+	magnitude: 1,
+	displacement: [1, 0],
+};
+var isPaused = true;
+
+var refreshID;
+var directionDict = {
+	up: [0, -1],
+	down: [0, 1],
+	right: [1, 0],
+	left: [-1, 0],
+};
 
 function randomPos() {
 	return {
@@ -48,5 +72,46 @@ function drawSnake() {
 	snake.body.forEach(({ x, y }) => context.fillRect(x * 10, y * 10, 10, 10));
 }
 
-drawApple();
-drawSnake();
+function startGame() {
+	drawApple();
+	drawSnake();
+
+	refreshID = setInterval(() => refresh(), 500);
+	isPaused = false;
+}
+
+function refresh() {
+	context.clearRect(0, 0, 500, 500);
+
+	moveSnake();
+
+	drawApple();
+	drawSnake();
+}
+
+function play() {
+	refreshID = setInterval(() => refresh(), 500);
+	isPaused = false;
+}
+
+function pause() {
+	clearInterval(refreshID);
+	isPaused = true;
+}
+
+function moveSnake() {
+	const clonedBody = [...snake.body];
+	clonedBody.forEach((segment) => {
+		segment.x += vector.displacement[0];
+		segment.y += vector.displacement[1];
+	});
+	snake = {
+		body: clonedBody,
+	};
+}
+
+function changeDirection(input) {
+	vector.displacement = directionDict[input];
+}
+
+startGame();
